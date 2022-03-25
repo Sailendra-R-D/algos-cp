8fa9b3307228db44ca3ccc90ab4d82599b473d62
Sailendra <sailendra.donthineni@gmail.com>
2022-03-25 17:58:44 +0530

    added bit manipulation scr for cp min xor value

--- Algos-interviewbit/src/main/java/com/algos/interviewbit/AlgoRunner.java
+++ Algos-interviewbit/src/main/java/com/algos/interviewbit/AlgoRunner.java
@@ -1,14 +1,20 @@
 package com.algos.interviewbit;
 
-import com.algos.interviewbit.bit.manipulation.Number_of_1_Bits.Number_of_1_Bits;
-import com.algos.interviewbit.bit.manipulation.Trailing_Zeroes.Trailing_Zeroes;
+import com.algos.interviewbit.bit.manipulation.Min_XOR_value.Min_XOR_value;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AlgoRunner {
     public static void main(String[] args) {
 //        Number_of_1_Bits number_of_1_bits = new Number_of_1_Bits();
 //        System.out.println("setBits :: "+ number_of_1_bits.numSetBits(7));
 
-        Trailing_Zeroes trailing_zeroes = new Trailing_Zeroes();
-        System.out.println(trailing_zeroes.solve(8));
+//        Trailing_Zeroes trailing_zeroes = new Trailing_Zeroes();
+//        System.out.println(trailing_zeroes.solve(8));
+
+        Min_XOR_value min_xor_value = new Min_XOR_value();
+        System.out.println(min_xor_value.findMinXor(new ArrayList<>(Arrays.asList(0, 2, 5, 7))));
+
     }
 }
---
+++ Algos-interviewbit/src/main/java/com/algos/interviewbit/bit/manipulation/Min_XOR_value/Min_XOR_value.java
@@ -0,0 +1,17 @@
+package com.algos.interviewbit.bit.manipulation.Min_XOR_value;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Min_XOR_value {
+    public int findMinXor(ArrayList<Integer> A) {
+        int mina = Integer.MAX_VALUE;
+        int i = 0;
+        Collections.sort(A);
+        while (i < A.size() - 1) {
+            mina = Math.min(mina, A.get(i) ^ A.get(i + 1));
+            i++;
+        }
+        return mina;
+    }
+}
