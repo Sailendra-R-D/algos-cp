e86e47d3639e003c5ec7681b41d96e4a02f709bb
Sailendra <sailendra.donthineni@gmail.com>
2022-03-25 17:14:01 +0530

    added bit manipulation scr for cp trailing zeroes

--- Algos-interviewbit/src/main/java/com/algos/interviewbit/AlgoRunner.java
+++ Algos-interviewbit/src/main/java/com/algos/interviewbit/AlgoRunner.java
@@ -1,12 +1,14 @@
 package com.algos.interviewbit;
 
 import com.algos.interviewbit.bit.manipulation.Number_of_1_Bits.Number_of_1_Bits;
+import com.algos.interviewbit.bit.manipulation.Trailing_Zeroes.Trailing_Zeroes;
 
 public class AlgoRunner {
     public static void main(String[] args) {
+//        Number_of_1_Bits number_of_1_bits = new Number_of_1_Bits();
+//        System.out.println("setBits :: "+ number_of_1_bits.numSetBits(7));
 
-        Number_of_1_Bits number_of_1_bits = new Number_of_1_Bits();
-
-        System.out.println("setBits :: "+ number_of_1_bits.numSetBits(7));
+        Trailing_Zeroes trailing_zeroes = new Trailing_Zeroes();
+        System.out.println(trailing_zeroes.solve(8));
     }
 }
---
+++ Algos-interviewbit/src/main/java/com/algos/interviewbit/bit/manipulation/Trailing_Zeroes/Trailing_Zeroes.java
@@ -0,0 +1,16 @@
+package com.algos.interviewbit.bit.manipulation.Trailing_Zeroes;
+
+public class Trailing_Zeroes {
+    public int solve(int a) {
+        int c = 0;
+        while (a > 0) {
+            if ((a & 1) == 0) {
+                c++;
+            } else {
+                break;
+            }
+            a = a >> 1;
+        }
+        return c;
+    }
+}
